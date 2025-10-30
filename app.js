986b08d595939dc369efaed67d0d4c48b22c13d5
ayankhan1213 <aw1800606@gmail.com>
2025-10-29 22:20:41 -0700

    JS Assignment 1

---
+++ app.js
@@ -0,0 +1,24 @@
+// Question 1 :
+// alert("Welcome to our Website");
+
+// Question 2 :
+// alert("Error! Please enter a valid password.")
+
+// Question 3 :
+// alert("Welcome to JS Land... \n Happy Coding!")
+
+// Question 4:
+// alert("Welcome to JS Land...")
+
+// Question 5 :
+// alert("Happy Coding! \n Prevent this page from creating additional dialogs.")
+
+// Question 6 :
+// alert("Hello... I can run JS through my web browser's console")
+
+// Question 7 :
+// alert("Welcome to our Apple Website 👀")
+
+
+
+
