4beb341a2d029a767134ded1edfc5315ba934460
ayankhan1213 <aw1800606@gmail.com>
2025-10-29 22:55:43 -0700

    JS Assignment # 1

---
+++ app.js
@@ -0,0 +1,23 @@
+// Question 1 :
+// alert("Hello World!")
+
+// Question 2 :
+// alert("Error! Please enter a valid password.")
+
+// Question 3 :
+// alert("Welcome to JS Land... \n Happy Coding!")
+
+// Question 4 :
+// alert("Welcome to JS Land")
+// alert("Happy Coding! \n Prevent this page from creating additional dialogs.")
+
+// Question 5 :
+// Web Browser Console Question
+// alert("Hello i can run JS through my web browser console.")
+
+// Question 6 :
+// alert("Welcome to our Website")
+
+// Question 7 :
+// Practice Script Tag 
+// Html Page👈
